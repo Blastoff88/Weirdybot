45312e1bd81fd17eb37f716e307dfa83f5b43e23
Joe Kutner <jpkutner@gmail.com>
2020-03-29 16:45:18 -0500

    change list name

--- list.py
+++ list.py
@@ -7,5 +7,5 @@ client = slack.WebClient(token=token)
 users = client.users_list() 
 for u in users["members"]:
     # print(u["name"])
-    if u["name"] == "weirdybot":
+    if u["name"] == "joekutner":
         print(f'{u["name"]} id is {u["id"]}')
