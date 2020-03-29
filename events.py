de7c18b20de2a80d9d3b1c33d417da3621636266
Joe Kutner <jpkutner@gmail.com>
2020-03-29 09:33:39 -0500

    set host

--- events.py
+++ events.py
@@ -41,4 +41,4 @@ def error_handler(err):
 # Once we have our event listeners configured, we can start the
 # Flask server with the default `/events` endpoint on port 3000
 PORT = os.environ["PORT"]
-slack_events_adapter.start(port=PORT)
+slack_events_adapter.start(host="0.0.0.0", port=PORT)
